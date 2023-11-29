79800f72f79aff476aa4587013ed2d037f81a2ae
msch32 <mirkoschultheiss@nigra-web.de>
2023-11-29 15:05:13 +0100

    added install/update script for sddm-theme

--- .config/sddm/setup.sh
+++ .config/sddm/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-sudo pacman -S sddm-sugar-candy-git
-sudo ln -s /usr/share/sddm/themes/sugar-candy/backgrounds ~/.config/sddm/backgrounds
-sudo ln -x /usr/share/sddm/themes/sugar-candy/theme.conf.user ~/.config/sddm/theme.conf.user
+#define the package name you want to check
+package_name="sddm-sugar-candy-git"
+
+if yay -Qs "$package_name" > /dev/null; then
+    echo "$package_name is already installed."
+else
+    #Install package, if not already installed
+    yay -S --noconfirm "$package_name"
+fi
+sudo cp backgrounds/wall-e.jpg /usr/share/sddm/themes/sugar-candy/Backgrounds/wall-e.jpg
+sudo cp theme.conf.user /usr/share/sddm/themes/sugar-candy/theme.conf.user
